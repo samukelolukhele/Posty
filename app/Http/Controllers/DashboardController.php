a2b9b25e7fadaa68f036c7479969dfff0d25bb68
samukelolukhele <samukelo.business@gmail.com>
2022-12-22 15:18:19 +0200

    Added likes & email notifications.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\PostLiked;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class DashboardController extends Controller
 {
     public function index(){
+      
         return view('dashboard');
     }
 }
---
+++ resources/views/emails/posts/post_liked.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+    
+# Your post was liked!
+
+{{ $liker->name }} liked one of your posts.
+
+@component('mail::button', ['url'=> route('posts.show', $post)])
+ View post
+@endcomponent
+
+    
+    Thanks,
+    {{ config('app.name') }}
+
+@endcomponent
